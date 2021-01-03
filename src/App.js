62893ad2c42923445241e67c0b471f8c00cc67a5
xenomech <gokulsmenon227@gmail.com>
2021-01-04 01:40:34 +0530

    ✅ test

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import Home from "./components/Home";
 import { Switch, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 
-
-// import RecentWorks from "./components/RecentWorks";
-// import svg from "./assets/Rectangle.svg";
-// import Sidenav from "./components/Sidenav";
-
 function App() {
   const currentlocation = useLocation();
   return (
@@ -16,30 +11,8 @@ function App() {
       <AnimatePresence exitBeforeEnter>
         <Switch location={currentlocation} key={currentlocation.pathname}>
           <Route path="/" exact component={Home} />
-          {/* <Route path="/works" exact component={RecentWorks} /> */}
         </Switch>
       </AnimatePresence>
-
-
-
-
-
-      {/* <div>
-        <div className="w-full flex p-5 justify-center items-center text-xl">
-          <p>
-            This Website was created using{" "}
-            <a className="text-blue-500" href="https://reactjs.org/">
-              React
-            </a>
-            .
-          </p>
-        </div>
-        <footer className="w-full flex justify-center items-center text-gray-900">
-          <FaCopyright className="m-3" />
-          <p>2020 Gokul Suresh</p>
-          <FaGithub className="m-3" />
-        </footer>
-      </div> */}
     </div>
   );
 }
